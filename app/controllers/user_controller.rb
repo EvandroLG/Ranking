ebb6365826210ddc1081a578e63e86416ef69fab
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2017-01-23 11:25:01 -0200

    add logic to user#update controller

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,8 +6,16 @@ class UserController < ApplicationController
   end
 
   def edit
+    @user = User.find params[:id]
   end
 
   def update
+    user = User.find params[:user][:id]
+    update = user.update name: params[:user][:name],
+									       points: params[:user][:points]
+
+    if update
+      redirect_to '/'
+    end
   end
 end
