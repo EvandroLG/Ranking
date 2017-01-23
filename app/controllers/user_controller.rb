8123e034aa0cb2e344712328385f9324a3ab43c8
Evandro Leopoldino Gonalves <evandrolgoncalves@gmail.com>
2017-01-23 11:05:36 -0200

    create user controller

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,13 @@
+class UserController < ApplicationController
+  def create
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post 'user/create'
+  get 'user/new'
+  get 'user/edit'
+  patch 'user/update'
 end
